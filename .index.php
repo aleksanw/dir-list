48788b63d7c4a1c75b42a4846bf920da8c50d51c
Aleksander Wasaznik <aleksanw@stud.ntnu.no>
2016-02-15 23:27:40 +0100

    Simplify dotfile check

--- .index.php
+++ .index.php
@@ -1,142 +1,73 @@
 <?php
 
-// Adds pretty filesizes
-function pretty_filesize($file) {
-	$size=filesize($file);
-	if($size<1024){$size=$size." Bytes";}
-	elseif(($size<1048576)&&($size>1023)){$size=round($size/1024, 1)." KB";}
-	elseif(($size<1073741824)&&($size>1048575)){$size=round($size/1048576, 1)." MB";}
-	else{$size=round($size/1073741824, 1)." GB";}
-	return $size;
-}
+	$directory = "./" . $_SERVER['PATH_INFO'];
 
-$directory = "./" . $_SERVER['PATH_INFO'];
 ?>
 <!doctype html>
-	<link rel="stylesheet" href="./.style.css">
-	<script src="./.sorttable.js"></script>
-
-<div id="container">
-	<h1>Directory Contents</h1>
-
-	<table class="sortable">
-		<th>Filename
-		<th>Type
-		<th>Size
-		<th>Date Modified
-<?php
-
-
-	$hide=".";
-	 $ahref="./?hidden";
-	 $atext="Show";
-
-	 // Opens directory
-	 $myDirectory=opendir($directory);
-
-	// Gets each entry
-	while($entryName=readdir($myDirectory)) {
-	   $dirArray[]=$entryName;
+<style>
+	* {
+		margin: 0;
 	}
 
-	// Closes directory
-	closedir($myDirectory);
+	body {
+		color: #333;
+		background: #eee;
+		font-family: sans-serif;
+	}
 
-	// Counts elements in array
-	$indexCount=count($dirArray);
+	h1 {
+		text-align: center;
+		padding: 1em 0 .5em 0;
+		margin: 0;
+	}
 
-	// Sorts files
-	sort($dirArray);
+	table {
+		width: 100%;
+		padding: 0 1em;
+		background-color: #f3f3f3;
+		border: collapse;
+	}
 
-	// Loops through the array of files
-	for($index=0; $index < $indexCount; $index++) {
+	th {
+		padding: .3em .7em;
+		background-color: #fe4902;
+		color: #fff;
+		text-align: left;
+	}
 
-	// Decides if hidden files should be displayed, based on query above.
-	    if(substr("$dirArray[$index]", 0, 1)!=$hide) {
+	tr:nth-of-type(odd) {
+		background-color: #E6E6E6;
+	}
 
-	// Resets Variables
-		$favicon="";
-		$class="file";
+	td {
+		padding: .3em .5em;
+	}
 
-	// Gets File Names
-		$name=$dirArray[$index];
-		$namehref=$dirArray[$index];
+	a {
+		text-decoration: none;
+	}
 
-	// Gets Date Modified
-		$modtime=date("M j Y g:i A", filemtime($dirArray[$index]));
-		$timekey=date("YmdHis", filemtime($dirArray[$index]));
+</style>
 
 
-	// Separates directories, and performs operations on those directories
-		if(is_dir($dirArray[$index]))
-		{
-				$extn="&lt;Directory&gt;";
-				$size="&lt;Directory&gt;";
-				$sizekey="0";
-				$class="dir";
+	<h1>Directory Contents</h1>
 
-			// Gets favicon.ico, and displays it, only if it exists.
-				if(file_exists("$namehref/favicon.ico"))
-					{
-						$favicon=" style='background-image:url($namehref/favicon.ico);'";
-						$extn="&lt;Website&gt;";
-					}
+	<table>
+		<th>Filename
 
-			// Cleans up . and .. directories
-				if($name=="."){$name=". (Current Directory)"; $extn="&lt;System Dir&gt;"; $favicon=" style='background-image:url($namehref/.favicon.ico);'";}
-				if($name==".."){$name=".. (Parent Directory)"; $extn="&lt;System Dir&gt;";}
-		}
+<?php
+	$contents = scandir($directory, SCANDIR_SORT_ASCENDING);
 
-	// File-only operations
-		else{
-			// Gets file extension
-			$extn=pathinfo($dirArray[$index], PATHINFO_EXTENSION);
+	foreach ($contents as $entry) {
+		// Hide entries starting with '.', ls-style.
+		// Except for '..' - thats useful.
+		if ($entry[0] === '.' && $entry !== '..')
+			continue;
 
-			// Prettifies file type
-			switch ($extn){
-				case "png": $extn="PNG Image"; break;
-				case "jpg": $extn="JPEG Image"; break;
-				case "jpeg": $extn="JPEG Image"; break;
-				case "svg": $extn="SVG Image"; break;
-				case "gif": $extn="GIF Image"; break;
-				case "ico": $extn="Windows Icon"; break;
+?>
+		<tr><td><a href="<?=htmlspecialchars($entry)?>">
+			<?=htmlspecialchars($entry)?>
 
-				case "txt": $extn="Text File"; break;
-				case "log": $extn="Log File"; break;
-				case "htm": $extn="HTML File"; break;
-				case "html": $extn="HTML File"; break;
-				case "xhtml": $extn="HTML File"; break;
-				case "shtml": $extn="HTML File"; break;
-				case "php": $extn="PHP Script"; break;
-				case "js": $extn="Javascript File"; break;
-				case "css": $extn="Stylesheet"; break;
-
-				case "pdf": $extn="PDF Document"; break;
-				case "xls": $extn="Spreadsheet"; break;
-				case "xlsx": $extn="Spreadsheet"; break;
-				case "doc": $extn="Microsoft Word Document"; break;
-				case "docx": $extn="Microsoft Word Document"; break;
-
-				case "zip": $extn="ZIP Archive"; break;
-				case "htaccess": $extn="Apache Config File"; break;
-				case "exe": $extn="Windows Executable"; break;
-
-				default: if($extn!=""){$extn=strtoupper($extn)." File";} else{$extn="Unknown";} break;
-			}
-
-			// Gets and cleans up file size
-				$size=pretty_filesize($dirArray[$index]);
-				$sizekey=filesize($dirArray[$index]);
-		}
-
-	// Output
-	 echo("
-		<tr class='$class'>
-			<td><a href='./$namehref'$favicon class='name'>$name
-			<td><a href='./$namehref'>$extn
-			<td sorttable_customkey='$sizekey'><a href='./$namehref'>$size
-			<td sorttable_customkey='$timekey'><a href='./$namehref'>$modtime
-		</tr>");
-	   }
+<?php
 	}
 ?>
